64f6151273eae1b52324b56362724baf273d26c6
bakou912 <tom912_a_@hotmail.com>
2021-12-02 22:54:33 -0500

    uncommenting in driver

--- src/Client/AdminDriver.java
+++ src/Client/AdminDriver.java
@@ -22,16 +22,16 @@ public class AdminDriver {
 
         try {
             AdminClient testClient1 = new AdminClient(aid1);
-//            AdminClient testClient2 = new AdminClient(aid2);
-//            AdminClient testClient3 = new AdminClient(aid3);
+            AdminClient testClient2 = new AdminClient(aid2);
+            AdminClient testClient3 = new AdminClient(aid3);
 
             //testing synchronization with multiple admins
             testClient1.createRoom(201, "01/01/2020", listOfTimeSlots);
-//            testClient3.createRoom(201, "01/01/2020", listOfTimeSlots);
-//            testClient1.createRoom(201, "03/01/2020", listOfTimeSlots2);
-//            testClient2.createRoom(211, "04/01/2020", listOfTimeSlots);
-//            testClient2.deleteRoom(211, "04/01/2020", listOfTimeSlots3);
-//            testClient3.createRoom(203, "01/01/2020", listOfTimeSlots);
+            testClient3.createRoom(201, "01/01/2020", listOfTimeSlots);
+            testClient1.createRoom(201, "03/01/2020", listOfTimeSlots2);
+            testClient2.createRoom(211, "04/01/2020", listOfTimeSlots);
+            testClient2.deleteRoom(211, "04/01/2020", listOfTimeSlots3);
+            testClient3.createRoom(203, "01/01/2020", listOfTimeSlots);
 
 
 //            testing invalid admin
